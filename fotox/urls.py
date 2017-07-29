13d7fc8b8b13df01199e900d1ab8952ade6c7191
yunica <freesoftandskate@gmail.com>
2017-07-29 02:03:31 -0500

    mejorando template

--- fotox/urls.py
+++ fotox/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^agregar/', views.agregar, name='agregar'),
+    url(r'^listar/', views.mostrar, name='mostrar'),
+    url(r'^listar2/(?P<pedido>[0-9]+)/$', views.mostrar2, name='mostrar2'),
 ]
 
--- fotox/views.py
+++ fotox/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
+from .models import Foto
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index :3.")
+
 
 def agregar(request):
     context = {
@@ -13,6 +15,15 @@ def agregar(request):
     return render(request, 'fotox/index.html', context)
 
 
+def mostrar(request):
+    lista = Foto.objects.all()
+    return render(request, 'fotox/pagina2.html', {'list': lista})
 
 
+def mostrar2(request, pedido):
+    try:
+        lista = Foto.objects.get(apellido=pedido)
+    except Foto.DoesNotExist:
+        raise Http404('Este Id no fue encontrado :( ')
 
+    return render(request, 'fotox/pagina2.html', {'list': lista})
